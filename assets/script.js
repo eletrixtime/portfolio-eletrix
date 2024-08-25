e2a4ff6627ab59f9a900835f87152596b3137d38
EletrixTime <71174682+eletrixtime@users.noreply.github.com>
2024-08-25 16:04:46 +0200

    V3
    V3

---
+++ assets/script.js
@@ -0,0 +1,1 @@
+const konamiCode=["r","i","p","n","u","n","o","b"];let konamiIndex=0;function handleKonamiCode(){++konamiIndex===konamiCode.length&&(alert("Nuno the cat : 2010-23/04/2024 \uD83E\uDEA6"),konamiIndex=0)}document.addEventListener("keydown",function(n){let e=n.key.toLowerCase();e===konamiCode[konamiIndex]?handleKonamiCode():konamiIndex=0});
